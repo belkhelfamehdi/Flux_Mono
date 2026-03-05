a85b8855dd7989163e71d9fb96544f2b00e41894
Mehdi Belkhelfa <midou8687@gmail.com>
2026-03-05 15:31:40 +0100

    done

---
+++ src/main/java/org/example/flux_mono/FluxMonoApplication.java
@@ -0,0 +1,13 @@
+package org.example.flux_mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxMonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/flux_mono/FluxMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flux_mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
